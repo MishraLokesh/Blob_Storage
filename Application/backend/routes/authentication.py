d646aceaed1fed67cd8196c72dbee795fcf8e45d
MishraLokesh <4man.mishra@gmail.com>
2022-01-31 10:50:24 +0530

    added HTTP exceptions to login endpoint

--- Application/backend/routes/authentication.py
+++ Application/backend/routes/authentication.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from schemas.user import Login
 from sqlalchemy.sql import text
@@ -20,14 +20,22 @@ def verify_password(plain_password, hashed_password):
 @router.post('/login')
 def login(login:OAuth2PasswordRequestForm = Depends()):
   s1 = text("SELECT * FROM blob.users where email = :username")
+  user = conn.execute(s1, username=login.username).fetchall()
+  if not user:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Invalid Credentials",)
+  
   q = text("SELECT password FROM blob.users where email = :username")
   hashed_pass = conn.execute(q, username=login.username).fetchall()[0][0]
-  if verify_password(login.password, hashed_pass):
-    # return conn.execute(s1, username=login.username).fetchall()
-  
-    # generate a JWT token and return it
-    access_token = JWT_token.create_access_token(data={"sub": login.username})
-    return {"access_token": access_token, "token_type": "bearer"}
+  if not verify_password(login.password, hashed_pass):
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Incorrect Password",)
+
+  # generate a JWT token and return it
+  access_token = JWT_token.create_access_token(data={"sub": login.username})
+  return {"access_token": access_token, "token_type": "bearer"}
 
 
 
